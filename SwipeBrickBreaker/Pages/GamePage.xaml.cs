bfa38c59f0538b8a0032d69f131a69e34da6bc0f
Mohammad MDSA <samimd.77@gmail.com>
2018-02-02 12:55:09 +0330

    Add project files.

---
+++ SwipeBrickBreaker/Pages/GamePage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Graphics.Canvas.UI.Xaml;
+using SwipeBrickBreaker.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SwipeBrickBreaker.Pages
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class GamePage : Page
+	{
+		public Mouse Mouse { get; }
+
+		public GamePage()
+		{
+			Mouse = new Mouse();
+			this.InitializeComponent();
+		}
+
+		private void GameBoard_PointerMoved(object sender, PointerRoutedEventArgs e)
+		{
+			Mouse.SetPosition(e.GetCurrentPoint((UIElement)sender).Position);
+		}
+
+		private void GameBoard_PointerPressed(object sender, PointerRoutedEventArgs e)
+		{
+
+		}
+
+		private void GameBoard_PointerReleased(object sender, PointerRoutedEventArgs e)
+		{
+
+		}
+
+		private void GameBoard_Draw(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
+		{
+			args.DrawingSession.Clear(Colors.White);
+			args.DrawingSession.DrawLine(0, 0, 1000, 1000, Colors.Red);
+			args.DrawingSession.DrawCircle(Mouse.GetPosition, 20, Colors.Red);
+			args.DrawingSession.DrawText(Mouse.GetPosition.X + " " + Mouse.GetPosition.Y, 100, 1000, Colors.Red);
+		}
+	}
+}
---
+++ SwipeBrickBreaker/Utils/Mouse.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace SwipeBrickBreaker.Utils
+{
+	public class Mouse
+	{
+		private Point Position;
+		public Vector2 GetPosition
+		{
+			get { return new Vector2((float)Position.X, (float)Position.Y); }
+		}
+
+
+		public Mouse()
+		{
+			Position.X = Position.Y = 0;
+		}
+
+		public void SetPosition(double x, double y)
+		{
+			Position.X = x;
+			Position.Y = y;
+		}
+
+		public void SetPosition(Vector2 vector)
+		{
+			this.SetPosition(vector.X, vector.Y);
+		}
+
+		public void SetPosition(Point point)
+		{
+			SetPosition(point.X, point.Y);
+		}
+	}
+
+	public struct Drager
+	{
+		public readonly Point StartPosition;
+		public Point CurentPosition { get; }
+
+		public Drager(Point start)
+		{
+			this.StartPosition = new Point(start.X, start.Y);
+			this.CurentPosition = new Point(start.X, start.Y);
+		}
+
+		//public Drager
+	}
+}
